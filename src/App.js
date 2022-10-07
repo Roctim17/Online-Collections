e73e44191abcfe3372bb4cb9c8ffa9245cb70cc9
roctim17m <mdrokibulislamroctim@gmail.com>
2022-10-07 21:27:44 +0600

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div >
+      <Navbar></Navbar>
+      <Home></Home>
 
     </div>
   );
