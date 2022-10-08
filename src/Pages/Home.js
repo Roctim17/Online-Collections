d5baf51383528235d41e9a25d162cbc6f90926aa
roctim17m <mdrokibulislamroctim@gmail.com>
2022-10-09 02:10:16 +0600

    done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,13 +4,13 @@ import Products from '../Components/Products';
 const Home = () => {
     return (
         <div className='hero'>
-            <div className="card bg-dark text-white border-0">
-                <img src="..." className="card-img" alt="..." 
+            <div className="card bg-dark border-0">
+                <img src="https://blog-assets.freshworks.com/freshsales-crm/wp-content/uploads/2018/11/Banner_002.png" className="card-img" alt="..." 
                 height="550px"/>
                 <div className="card-img-overlay d-flex flex-column justify-content-center">
                     <div className='container'>
-                    <h5 className="card-title display-3 fw-border mb-0">NEW SEASON</h5>
-                    <p className="card-text lead fs-2">
+                    <h5 className="card-title display-3 fw-bolder mb-0 ">NEW SEASON</h5>
+                    <p className="card-text lead fw-bold fs-2">
                         CHECK OUT ALL THE TRENDS
                     </p>
                     </div>
