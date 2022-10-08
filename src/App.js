e3d29f2eec8416c8f51475ba18b50c7bf7963de7
roctim17m <mdrokibulislamroctim@gmail.com>
2022-10-08 17:04:23 +0600

    done latest products

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import {Route,  Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar';
+import Products from './Components/Products';
 import Home from './Pages/Home';
 
 function App() {
   return (
     <div >
       <Navbar></Navbar>
-      <Home></Home>
-
+      <Routes>
+      <Route  path='/' element={<Home/>}></Route>
+      <Route  path='/products' element={<Products/>}></Route>
+      </Routes>
     </div>
   );
 }
